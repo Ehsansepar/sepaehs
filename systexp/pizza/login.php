ea294b972dd82ac7d346b74127821a7c2b994cd1
Ehsan SEPAR <100959074+Ehsansepar@users.noreply.github.com>
2025-01-30 22:31:52 +0100

    Refactor login.php with improved authentication logic and form handling

--- systexp/pizza/login.php
+++ systexp/pizza/login.php
@@ -1,44 +1,33 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="post">
-        <input type="email" name="email" id="email" require>
-        <input type="password" name="password" id="password" require>
-    </form>
-</body>
-</html>
-
-
 <?php
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $email = htmlspecialchars($_POST['email']);
-    $password = hash('sha256', htmlspecialchars($_POST['password']));
-
+if(isset($_POST['mail'])) {
+    session_start();
+    $email = htmlspecialchars($_POST['mail']);
+    $password = hash('sha256', htmlspecialchars($_POST['mdp']));
 
     require_once("secure/dbConnect.php");
-
-    try {
-
+    $request = $dbPizza->prepare("SELECT * FROM Utilisateurs
+                       WHERE email = :email AND password = :password");
+    $request->execute(array(
+        ":email" => $email,
+        ":password" => $password
+    ));
+    $results = $request->fetchAll();
+    echo $results;
+    echo count($results);
+    if(count($results) == 1) {
+        //Ok pour la connexion
+        $_SESSION["email"] = $email;
+        header('Location: index.php');
+        die();
     }
-    catch (Exception $e) {
-        echo "Veuillez resseayer <br>";
-    }
+    echo "Mauvaise combinnaison de login et mot de passe";
 
-// Récupere les données du formulaire si il a ete rempli
+}
 
-// Verifier que le login correspond au mot de passe
-
-// si ok, rediriger vers index.php
-// si pas ok => message d erreur mais afficher quand meme le formulaire de connexion
-
-
-//Formulaire de connexion
 ?>
+
+<form method="post" action="">
+    Mail : <input type="email" name="mail" value="<?php echo $_POST['mail'] ?? "";?>"><br>
+    Mot de passe: <input type="password" name="mdp" value="<?php echo $_POST['mdp'] ?? "";?>"><br>
+    <button type="submit">Inscription</button>
+</form>
